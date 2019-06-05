ece35030160dd9e632051ab89a8d4d50fac2b44b
Matej01 <49277350+Matej01@users.noreply.github.com>
2019-06-05 11:21:05 +0200

    new2

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,18 @@
-console.log( 'JS loaded' )
+console.log( 'JS loaded' );
+
+
+document.addEventListener( 'deviceready', function() {
+    console.log( 'Device ready' );
+
+    $( document ).ready( function() {
+        console.log( 'DOM ready, alles ready...' )
+        
+        navigator.geolocation.getCurrentPosition( function() {
+            $('<div>').html(cords.latitude + '/' + cords.longitude ).appendTo( 'body' );
+        },function() {
+            $('<div>').html('Error no position').appendTo( 'body' );
+        })
+
+    });
+
+});
