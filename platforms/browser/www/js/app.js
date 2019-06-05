419f66c3036cdf5dc380be8dad805fc18ed9e98a
Matej01 <49277350+Matej01@users.noreply.github.com>
2019-06-05 16:52:15 +0200

    newx

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,86 +1,94 @@
 console.log( 'JS loaded' );
+var superPaint = {
+  color:'#fff',
+  initPens:function() {
+    var colors = ['#000','#f00','#0f0','#00f','#ff0','#f0f','#0ff','#ccc','#fc3','#63c'];
+    for (let i in colors ) {
+      $( '<pen>' )
+        .css({background:colors[i]})
+        .on('click', this.activatePen(colors[i]) )
+        .appendTo( 'footer' );
+    }
+  },
+  activatePen:function( newColor ) {
+    return function() {
+      superPaint.color = newColor;
+      $( '.active' ).removeClass( 'active');
+      $( this ).addClass( 'active' );
+    }
+  },
+  drawModus: 'dot', /*line*/
+  drawStart: function(e) {
+    var offset = $('#stage').offset();
+    superPaint.oldX = e.touches[0].pageX - offset.left;
+    superPaint.oldY = e.touches[0].pageY - offset.top;
+    superPaint.draw(e);
+  },
+  draw: function(e) {
+    var offset = $('#stage').offset();
+    var x = e.touches[0].pageX - offset.left;
+    var y = e.touches[0].pageY - offset.top;
+    var ctx = superPaint.ctx;
+    ctx.fillStyle = ctx.strokeStyle = superPaint.color;
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    if ( superPaint.drawModus == 'dot' ) {
+      ctx.arc(x,y,10,0,2*Math.PI);
+      ctx.fill();
+    }
+    if ( superPaint.drawModus == 'line' ) {
+      ctx.moveTo(superPaint.oldX,superPaint.oldY);
+      ctx.lineTo(x,y);
+      ctx.stroke();
+      superPaint.oldX = x;
+      superPaint.oldY = y;
+    }
+
+  },
+  initCanvas:function() {
+    this.ctx = $('#stage').get(0).getContext('2d');
+    $( '#stage' )
+      .attr({
+        width:$(window).width(),
+        height:$(window).height()-80
+      })
+      .on('touchstart', this.drawStart )
+      .on('touchmove', this.draw );
+  },
+
+  clearAll:function() {
+    var ctx = superPaint.ctx;
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0,0,$('#stage').width(),$('#stage').height());
+  },
+  
+  saveData:function() {
+    window.canvas2ImagePlugin.saveImageDataToLibrary(
+      function(msg){
+        alert('Daten sind gespeichert');
+      },
+      function(err) {
+        alert('Fehler beim speichern')
+      },
+      $('#stage').get(0)
+    );
+    
+  },
+
+
+  init:function() {
+    this.initPens();
+    this.initCanvas();
+  }
+}
 
 
 document.addEventListener( 'deviceready', function() {
-    
-
-    $( document ).ready( function() {
-        var started = false;
-        var canvas, context;
-        var stampId = '';
-        var lastColor = 'black';
-        var lastStampId = '';
+  console.log( 'Device ready' );
+  $( document ).ready( function() {
+    console.log( 'DOM ready, alles ready...' );
+    superPaint.init();
 
 
-        function init() {
-            canvas = $('#imageView').get(0);
-            context = canvas.getContext('2d');
-
-            canvas.addEventListener('mousemove', onMouseMove, false);
-            canvas.addEventListener('click', onClick, false);
-
-
-            $('#red').get(0).addEventListener('click', function(e) { 
-                onColorClick(e.target.id); }, false);
-
-               $('#blue').get(0).addEventListener('click', function(e) { 
-                onColorClick(e.target.id); }, false);
-
-               $('#green').get(0).addEventListener('click', function(e) { 
-                onColorClick(e.target.id); }, false);
-
-                $('#yellow').get(0).addEventListener('click', function(e) { 
-                 onColorClick(e.target.id); }, false);
-
-                $('#black').get(0).addEventListener('click', function(e) { 
-                 onColorClick(e.target.id); }, false);
-
-                 function onMouseMove(ev) {
-                    var x,y;
-
-                    if (ev.offsetX >= 0) {
-                        
-                        x = ev.offsetX - 50;
-                        y = ev.offsetY - 5;
-                    }
-
-                    if (!started) {
-                        started = true;
-                        context.beginPath();
-                        context.moveTo(x, y);        
-                    }
-                    else {
-                        context.lineTo(x, y);
-                        context.stroke();
-                    }
-
-                 };
-
-
-                 function onClick(e) {
-                    if (stampId.length > 0) {
-                        context.drawImage($(stampId).get(0), e.pageX - 90, e.pageY - 60, 80, 80);
-                    }
-                }
-
-
-                function onColorClick(color) {
-                    
-                    context.closePath();
-                    context.beginPath();
-                    
-                    context.strokeStyle = color;
-                }
-
-                function onFill() {
-                    
-                    context.closePath();
-                    context.beginPath();
-                    context.fillStyle = context.strokeStyle;
-                    context.fillRect(0, 0, canvas.width, canvas.height);
-                }
-        };        
-
-    });
-
+  });
 });
