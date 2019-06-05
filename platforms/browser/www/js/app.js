12dc6a8e6db072e5c4320c0f78edef01261f70db
Matej01 <49277350+Matej01@users.noreply.github.com>
2019-06-05 11:45:38 +0200

    new3

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -8,7 +8,7 @@ document.addEventListener( 'deviceready', function() {
         console.log( 'DOM ready, alles ready...' )
         
         navigator.geolocation.getCurrentPosition( function() {
-            $('<div>').html(cords.latitude + '/' + cords.longitude ).appendTo( 'body' );
+            $('<div>').html(coords.coords.latitude + '/' + coords.coords.longitude ).appendTo( 'body' );
         },function() {
             $('<div>').html('Error no position').appendTo( 'body' );
         })
